ed1419fe02ef4ead556b09ab08f58fc645785a6a
Vinicius Cavalcanti <vincius.cavalcanti16@gmail.com>
2018-11-01 16:08:55 -0300

    adicionado: dashbaord de candidados

--- src/main/java/br/com/zonework/candidatedevs/candidate/application/endpoint/DashboardCandidateServlet.java
+++ src/main/java/br/com/zonework/candidatedevs/candidate/application/endpoint/DashboardCandidateServlet.java
@@ -1,4 +1,4 @@
-package br.com.zonework.candidatedevs.members.application.endpoint;
+package br.com.zonework.candidatedevs.candidate.application.endpoint;
 
 import br.com.zonework.candidatedevs.structure.Render;
 
@@ -9,11 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/members/dashboard")
-public class DashboardServlet extends HttpServlet {
-
+@WebServlet(urlPatterns = "/candidate/dashboard")
+public class DashboardCandidateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Render.view(req, resp, "members/dashboard");
+        Render.view(req, resp, "/candidate/dashboard");
     }
 }
